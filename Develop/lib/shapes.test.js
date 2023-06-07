1f1146fea99e2d17700e09a1ed23748263ec806d
Muramori71 <dannyroh2500@gmail.com>
2023-06-07 08:14:13 -0700

    updated shapes and test files

--- Develop/lib/shapes.test.js
+++ Develop/lib/shapes.test.js
@@ -2,7 +2,7 @@ const{circle, square, triangle}  = require('./shapes.js');
 
 describe( {circle}, () => {
   test('Creates a blue circle', () => {
-    const expectedCircle = '<circle cx="150" cy="100" r="80" fill="${this.shapeColor}" text x="150" y="125" font-size="60" text-anchor="middle" fill="${this.color}">SVG</text>/>'
+    const expectedCircle = '<circle cx="150" cy="100" r="80" fill="${this.shapeColor}" text x="150" y="125" font-size="60" text-anchor="middle" fill="${this.color}">${this.text}</text>/>'
     const testCircle = new circle;
     testCircle.fill("blue");
     testCircle = testCircle.render();
@@ -12,21 +12,21 @@ describe( {circle}, () => {
   
   describe( {square}, () => {
     test('Creates a blue square', () => {
-      const expectedCircle = '<rect x="10" y="10" width="30" height="30" stroke="black" fill="${this.shapeColor}" stroke-width="5" text x="150" y="125" font-size="60" text-anchor="middle" fill="${this.color}">SVG</text>/>'
-      const testCircle = new circle;
-      testCircle.fill("blue");
-      testCircle = testCircle.render();
-      expect(testCircle).toEqual(expectedCircle);
+      const expectedSquare = '<rect x="10" y="10" width="30" height="30" stroke="black" fill="${this.shapeColor}" stroke-width="5" text x="150" y="125" font-size="60" text-anchor="middle" fill="${this.color}">${this.text}</text>/>'
+      const testSquare = new circle;
+      testSquare.fill("blue");
+      testSquare = testSquare.render();
+      expect(testSquare).toEqual(expectedSquare);
     });
     })  
 
     describe( {triangle}, () => {
-      test('Creates a blue circle', () => {
-        const expectedCircle = '<polygon points="200,10 250,190 160,210" style="fill:${this.shapeColor};stroke:purple;stroke-width:1" <text x="150" y="125" font-size="60" text-anchor="middle" fill="${this.color}">SVG</text>/>'
-        const testCircle = new circle;
-        testCircle.fill("blue");
-        testCircle = testCircle.render();
-        expect(testCircle).toEqual(expectedCircle);
+      test('Creates a blue triangle', () => {
+        const expectedTriangle = '<polygon points="200,10 250,190 160,210" style="fill:${this.shapeColor};stroke:purple;stroke-width:1" <text x="150" y="125" font-size="60" text-anchor="middle" fill="${this.color}">${this.text}</text>/>'
+        const testTriangle = new circle;
+        testTriangle.fill("blue");
+        testTriangle = testTriangle.render();
+        expect(testTriangle).toEqual(expectedTriangle);
       });
       })  
 
