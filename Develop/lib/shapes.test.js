67a03bffd8ef92947c29dbd2659db7bfa792a7e5
Muramori71 <dannyroh2500@gmail.com>
2023-06-07 08:19:55 -0700

    updated test file

--- Develop/lib/shapes.test.js
+++ Develop/lib/shapes.test.js
@@ -4,7 +4,6 @@ describe( {circle}, () => {
   test('Creates a blue circle', () => {
     const expectedCircle = '<circle cx="150" cy="100" r="80" fill="${this.shapeColor}" text x="150" y="125" font-size="60" text-anchor="middle" fill="${this.color}">${this.text}</text>/>'
     const testCircle = new circle;
-    testCircle.fill("blue");
     testCircle = testCircle.render();
     expect(testCircle).toEqual(expectedCircle);
   });
@@ -13,8 +12,7 @@ describe( {circle}, () => {
   describe( {square}, () => {
     test('Creates a blue square', () => {
       const expectedSquare = '<rect x="10" y="10" width="30" height="30" stroke="black" fill="${this.shapeColor}" stroke-width="5" text x="150" y="125" font-size="60" text-anchor="middle" fill="${this.color}">${this.text}</text>/>'
-      const testSquare = new circle;
-      testSquare.fill("blue");
+      const testSquare = new square;
       testSquare = testSquare.render();
       expect(testSquare).toEqual(expectedSquare);
     });
@@ -23,8 +21,7 @@ describe( {circle}, () => {
     describe( {triangle}, () => {
       test('Creates a blue triangle', () => {
         const expectedTriangle = '<polygon points="200,10 250,190 160,210" style="fill:${this.shapeColor};stroke:purple;stroke-width:1" <text x="150" y="125" font-size="60" text-anchor="middle" fill="${this.color}">${this.text}</text>/>'
-        const testTriangle = new circle;
-        testTriangle.fill("blue");
+        const testTriangle = new triangle;
         testTriangle = testTriangle.render();
         expect(testTriangle).toEqual(expectedTriangle);
       });
