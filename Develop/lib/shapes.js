e8d174f0575f0dd7a420cc4c7941dc8361dca108
Muramori71 <dannyroh2500@gmail.com>
2023-06-08 08:19:16 -0700

    updated files

--- Develop/lib/shapes.js
+++ Develop/lib/shapes.js
@@ -8,7 +8,7 @@ class shape {
 }
 class circle extends shape {
 
-    //<text x="150" y="125" font-size="60" text-anchor="middle" fill="${this.color}">${this.text}</text>
+    
     render() {
         return `<circle cx="150" cy="100" r="80" fill="${this.shapeColor}" />`
     }
@@ -19,7 +19,7 @@ class square extends shape {
     render() {
         return `
         
-       <rect x="10" y="10" width="30" height="30" stroke="black" fill="${this.shapeColor}" stroke-width="5"/>
+       <rect x="80" y="60" width="140" height="99" stroke="black" fill="${this.shapeColor}" stroke-width="5"/>
        
         
        `
@@ -30,7 +30,7 @@ class triangle extends shape {
     
     render() {
         return `
-        <polygon points="200,10 250,190 160,210" style="fill:${this.shapeColor};stroke:purple;stroke-width:1" />
+        <polygon points="150,10 -1,200 320,230" "fill:${this.shapeColor} style="fill:purple;stroke:purple;stroke-width:1/>
         
       `
     }
