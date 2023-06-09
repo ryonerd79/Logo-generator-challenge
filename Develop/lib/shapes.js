eb16f72be05507d9dcd2f92fb15688bea321aa5e
Muramori71 <dannyroh2500@gmail.com>
2023-06-09 10:22:58 -0700

    updated lib files

--- Develop/lib/shapes.js
+++ Develop/lib/shapes.js
@@ -17,22 +17,14 @@ class circle extends shape {
 class square extends shape {
     
     render() {
-        return `
-        
-       <rect x="78" y="30" width="140" height="678" stroke="black" fill="${this.shapeColor}" stroke-width="5"/>
-       
-        
-       `
+        return `<rect x="78" y="30" width="140" height="678" stroke="black" fill="${this.shapeColor}" stroke-width="5" />`
     }
 }
 
 class triangle extends shape {
     
     render() {
-        return `
-        <polygon points="150,10 -1,200 320,230" fill="${this.shapeColor}"/>
-        
-      `
+        return `<polygon points="150,10 -1,200 320,230" fill="${this.shapeColor}" />`
     }
 }
 
--- Develop/lib/shapes.test.js
+++ Develop/lib/shapes.test.js
@@ -1,16 +1,34 @@
-const{circle, square, triangle}  = require('./shapes.js');
+const { circle, square, triangle } = require('./shapes.js');
 
-describe( 'circle', () => {
+describe('circle', () => {
   test('Creates a blue circle', () => {
     const expectedCircle = '<circle cx="150" cy="115" r="80" fill="blue" />'
     const testCircle = new circle();
     testCircle.setShapeColor('blue');
-    const testSVG = testCircle.render();
-    expect(testSVG).toEqual(expectedCircle);
+    const testSVGCircleLogo = testCircle.render();
+    expect(testSVGCircleLogo).toEqual(expectedCircle);
   });
-  })
-  
-  
+})
+
+describe('square', () => {
+    test('Creates a blue square', () => {
+      const expectedSquare = '<rect x="78" y="30" width="140" height="678" stroke="black" fill="blue" stroke-width="5" />'
+      const testSquare = new square();
+      testSquare.setShapeColor('blue');
+      const testSVGSquareLogo = testSquare.render();
+      expect(testSVGSquareLogo).toEqual(expectedSquare);
+    });
+  }) 
+
+    describe('triangle', () => {
+      test('Creates a blue triangle', () => {
+        const expectedTriangle = '<polygon points="150,10 -1,200 320,230" fill="blue" />'
+        const testTriangle = new triangle;
+        testTriangle.setShapeColor('blue');
+        const testSVGTriangleLogo = testTriangle.render();
+        expect(testSVGTriangleLogo).toEqual(expectedTriangle);
+      });
+      })
 
 
 
