734a9eaa350a9232ed397a7818b9d8b9ebfcd83e
Muramori71 <dannyroh2500@gmail.com>
2023-06-09 08:22:18 -0700

    updated test file

--- Develop/lib/shapes.test.js
+++ Develop/lib/shapes.test.js
@@ -2,7 +2,7 @@ const{circle, square, triangle}  = require('./shapes.js');
 
 describe( 'circle', () => {
   test('Creates a blue circle', () => {
-    const expectedCircle = '<circle cx="150" cy="100" r="80" fill="blue" />'
+    const expectedCircle = '<circle cx="150" cy="115" r="80" fill="blue" />'
     const testCircle = new circle();
     testCircle.setShapeColor('blue');
     const testSVG = testCircle.render();
